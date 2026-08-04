e275c6ec62fbab147b941f7a92610ee6f7d86fbc
groceryhub3112-droid <groceryhub3112@gmail.com>
2026-08-04 22:57:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("WATER HUB 2.0 Loaded Successfully!");
+});
+
+function callNow() {
+  window.location.href = "tel:7866945566";
+}
+
+function whatsappNow() {
+  window.open("https://wa.me/917866945566", "_blank");
+}
