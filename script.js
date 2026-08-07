26bdc185e16fb3e0b3cb656c3a50653a6909e9d2
groceryhub3112-droid <groceryhub3112@gmail.com>
2026-08-07 22:32:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("WATER HUB 2.0 Loaded Successfully!");
+
+  updateCartCount();
 });
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+
+  cart.push({
+    name: name,
+    price: price,
+    qty: 1
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  updateCartCount();
+
+  alert(name + " Cart-এ যোগ হয়েছে ✅");
+}
+
+function updateCartCount() {
+
+  let count = document.getElementById("cart-count");
+
+  if (count) {
+    count.innerText = cart.length;
+  }
+
+}
 
 function callNow() {
   window.location.href = "tel:7866945566";
